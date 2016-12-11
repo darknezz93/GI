156c7594a5eef16f26908bf3dfc62c6e8dd3f9ac
Adam Szczesiak <darknezz2@wp.pl>
2016-12-11 15:16:32 +0100

    Suwak do wyboru roku

--- scripts.js
+++ scripts.js
@@ -7,5 +7,19 @@
             $box.prop("checked", false);
             alert("Można wybrać maksymalnie 4 kategorie");
              }
-    });             
- });
+    });
+
+    $("#slider").slider({
+        value: 100,
+        min: 0,
+        max: 500,
+        step: 100,
+        slide: function (event, ui) {
+            $("#amount").val("$" + ui.value);
+        }
+    });
+ 
+});
+
+
+
